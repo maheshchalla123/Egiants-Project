57e555ed6e3ae5f43835b4e66747a5611cd94294
maheshchalla123 <35147576+maheshchalla123@users.noreply.github.com>
2018-01-05 13:01:22 -0600

    Add files via upload

---
+++ Say-Hello/src/main/java/com/pivotal/SayHelloApplication.java
@@ -0,0 +1,40 @@
+package com.pivotal;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.*;
+
+@RestController
+@SpringBootApplication
+public class SayHelloApplication {
+
+  private static Logger log = LoggerFactory.getLogger(SayHelloApplication.class);
+ @Value("${server.port}")
+ String serverport;
+  @RequestMapping(value = "/greeting")
+  public String greet() {
+    log.info("Access /greeting");
+
+    List<String> greetings = Arrays.asList("Hi there", "Greetings", "venkata");
+    Random rand = new Random();
+
+    int randomNum = rand.nextInt(greetings.size());
+	return serverport + " : " + greetings.get(randomNum);
+  }
+
+  @RequestMapping(value = "/")
+  public String home() {
+    log.info("Access /");
+    return "Hi!";
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(SayHelloApplication.class, args);
+  }
+}
